aaf4a4bb4b43ff2f722847fd6a0ac3e488460af4
AlexJS6 <alex.spiesberger@gmail.com>
2020-08-25 12:21:13 +0200

    adding classes to images in category

--- category.php
+++ category.php
@@ -9,70 +9,70 @@
 <body>
     <header id="main-header" class="main-header">
         <img id="logo-small" class="logo-small" src="assets/pictures/sqrflix-logo-small.png" alt="SQRFLIX Logo">
-        <img id="header-picture"  class="hearder-picture" src="assets/pictures/shining.png" alt="The Shining">
+        <img id="header-picture"  class="hearder-picture image_category" src="assets/pictures/shining.png" alt="The Shining">
     </header>
     <section class="movies">
       <div class="grid-container">
 
         <!-- movie -->
         <figure class="movie">
-          <a href=""><img src="assets/pictures/gravity.png" alt="team member"></a>
+          <a href=""><img class= "image_category" src="assets/pictures/gravity.png" alt="team member"></a>
         </figure>
         <!-- movie -->
         <figure class="movie">
-          <a href=""><img src="assets/pictures/gravity.png" alt="team member"></a>
+          <a href=""><img class= "image_category" src="assets/pictures/gravity.png" alt="team member"></a>
         </figure>
         <!-- movie -->
         <figure class="movie">
-          <a href=""><img src="assets/pictures/gravity.png" alt="team member"></a>
+          <a href=""><img class= "image_category" src="assets/pictures/gravity.png" alt="team member"></a>
         </figure>
         <!-- movie -->
         <figure class="movie">
-          <a href=""><img src="assets/pictures/gravity.png" alt="team member"></a>
+          <a href=""><img class= "image_category" src="assets/pictures/gravity.png" alt="team member"></a>
         </figure>
         <!-- movie -->
         <figure class="movie">
-          <a href=""><img src="assets/pictures/gravity.png" alt="team member"></a>
+          <a href=""><img class= "image_category" src="assets/pictures/gravity.png" alt="team member"></a>
         </figure>
         <!-- movie -->
         <figure class="movie">
-          <a href=""><img src="assets/pictures/gravity.png" alt="team member"></a>
+          <a href=""><img class= "image_category" src="assets/pictures/gravity.png" alt="team member"></a>
         </figure>
         <!-- movie -->
         <figure class="movie">
-          <a href=""><img src="assets/pictures/gravity.png" alt="team member"></a>
+          <a href=""><img class= "image_category" src="assets/pictures/gravity.png" alt="team member"></a>
         </figure>
         <!-- movie -->
         <figure class="movie">
-          <a href=""><img src="assets/pictures/gravity.png" alt="team member"></a>
+          <a href=""><img class= "image_category" src="assets/pictures/gravity.png" alt="team member"></a>
         </figure>
         <!-- movie -->
         <figure class="movie">
-          <a href=""><img src="assets/pictures/gravity.png" alt="team member"></a>
+          <a href=""><img class= "image_category" src="assets/pictures/gravity.png" alt="team member"></a>
         </figure>
         <!-- movie -->
         <figure class="movie">
-          <a href=""><img src="assets/pictures/gravity.png" alt="team member"></a>
+          <a href=""><img class= "image_category" src="assets/pictures/gravity.png" alt="team member"></a>
         </figure>
         <!-- movie -->
         <figure class="movie">
-          <a href=""><img src="assets/pictures/gravity.png" alt="team member"></a>
+          <a href=""><img class= "image_category" src="assets/pictures/gravity.png" alt="team member"></a>
         </figure>
         <!-- movie -->
         <figure class="movie">
-          <a href=""><img src="assets/pictures/gravity.png" alt="team member"></a>
+          <a href=""><img class= "image_category" src="assets/pictures/gravity.png" alt="team member"></a>
         </figure>
         <!-- movie -->
         <figure class="movie">
-          <a href=""><img src="assets/pictures/gravity.png" alt="team member"></a>
+          <a href=""><img class= "image_category" src="assets/pictures/gravity.png" alt="team member"></a>
         </figure>
         <!-- movie -->
         <figure class="movie">
-          <a href=""><img src="assets/pictures/gravity.png" alt="team member"></a>
+          <a href=""><img class= "image_category" src="assets/pictures/gravity.png" alt="team member"></a>
         </figure>
         <!-- movie -->
         <figure class="movie">
-          <a href=""><img src="assets/pictures/gravity.png" alt="team member"></a>
+          <a href=""><img class= "image_category" src="assets/pictures/gravity.png" alt="team member"></a>
         </figure>
         
 
