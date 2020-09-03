9dbe305ed63f56a179c3f750451efe6aa17ca826
Maximilien <pierremaximilien38@gmail.com>
2020-09-03 16:42:16 +0200

    serv

--- data_base.php
+++ data_base.php
@@ -1,16 +1,16 @@
 <?php
 /*server version */
-
+/*
 try {
-    $DB = new PDO('mysql:host=sql301.epizy.com;dbname=sepiz_26653823_SQRFLIX;charset=utf8', 'epiz_26653823', 'N6ZTCmkO4B9', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+    $DB = new PDO('mysql:host=sql301.epizy.com;dbname=epiz_26653823_SQRFLIX;charset=utf8', 'epiz_26653823', 'N6ZTCmkO4B9', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
   }
   catch (Exception $e)
   {
     die ('Erreur ' . $e->getMessage());
   }
-
+*/
 /* local host version */
-/*
+
   try {
     $DB = new PDO('mysql:host=localhost;dbname=sqrflix;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
   }
@@ -18,5 +18,5 @@ try {
   {
     die ('Erreur ' . $e->getMessage());
   }
-*/
+
 ?>
